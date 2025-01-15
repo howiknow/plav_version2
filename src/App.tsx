69c5f065fc911cd165cb7ad933487c5eb3b132d6
howiknow <paksangwon0223@gmail.com>
2025-01-15 22:18:57 +0900

    모바일 화면 반응형 조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./Router"; 
+import router from "./Router";
 import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-screen max-w-[375px] min-w-[320px] mx-auto">
+    <div className="w-full h-screen max-w-[480px] min-w-[320px] mx-auto">
       <RouterProvider router={router} />
     </div>
   );
